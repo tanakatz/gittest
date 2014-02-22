ef577e07e555eb29fefa54b3cd67014cfc39b191
tanakatz <tianzhong365@gmail.com>
2014-02-22 16:20:48 +0800

    comment

--- xxt/src/hello.java
+++ xxt/src/hello.java
@@ -3,6 +3,6 @@ public class hello {
 	public static void main(String args[ ]){
 		System.out.println("hellsd");
 		System.out.println("say the world");
-		System.out.println("xxt");
+		//System.out.println("xxt");
 	}
 }
